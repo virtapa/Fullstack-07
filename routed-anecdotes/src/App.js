78cc65c2b1def913c1d8f7c547e866548c93d981
Panu Virta <panu.virta@student.hamk.fi>
2021-07-07 17:59:10 +0300

    7.4-7.6

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -8,6 +8,7 @@ import {
   Redirect,
   useHistory,
 } from 'react-router-dom'
+import  { useField } from './hooks/index'
 
 const Menu = () => {
   const padding = {
@@ -92,23 +93,28 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  //const [content, setContent] = useState('')
+  //const [author, setAuthor] = useState('')
+  //const [info, setInfo] = useState('')
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
-    setAuthor('')
-    setContent('')
-    setInfo('')
     history.push('/')
+  }
+  const resetForm = () => {
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -117,29 +123,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author}/>
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={resetForm}>reset</button>
       </form>
     </div>
   )
